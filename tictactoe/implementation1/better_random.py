8385454f6db818668b6d79915c99e97344d54142
Morgan Borjigin-Wang <mbwang10@gmail.com>
2022-01-30 10:13:45 -0600

    Clean up better_random.py using TTT impl 2 strategy

--- tictactoe/implementation1/better_random.py
+++ tictactoe/implementation1/better_random.py
@@ -1,101 +1,65 @@
 #!/usr/bin/env python3
-import random
 import sys
 
-EMPTY = 0
-ME = 1
-THEM = 100
 
-board = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
+def main():
+    empty_positions, my_positions, their_positions = set(range(9)), set(), set()
+    winning_positions = [
+        {0, 1, 2},
+        {3, 4, 5},
+        {6, 7, 8},
+        {0, 3, 6},
+        {1, 4, 7},
+        {2, 5, 8},
+        {0, 4, 8},
+        {2, 4, 6},
+    ]
 
+    def has_winning_position(positions: set):
+        return any([pos.issubset(positions) for pos in winning_positions])
 
-def calculate_winner():
-    lines = [row for row in board]
-    for col in range(0, 3):
-        lines.append([board[row][col] for row in range(0, 3)])
-    lines.append([board[x][x] for x in range(0, 3)])
-    lines.append([board[x][2 - x] for x in range(0, 3)])
-    for line in lines:
-        line_sum = sum(line)
-        if line_sum == ME * 3:
-            return ME
-        elif line_sum == THEM * 3:
-            return THEM
-    return EMPTY
+    def print_board():
+        for pos in range(9):
+            print(
+                "X" if pos in my_positions else "O" if pos in their_positions else "-",
+                end="\n" if pos % 3 == 2 else "",
+                file=sys.stderr,  # So we don't confuse the manager
+            )
 
+    # Return a winning move or -1 if no winning move exists
+    def move_that_wins() -> int:
+        for move in empty_positions:
+            my_future_positions = set(my_positions)
+            my_future_positions.add(move)
+            if has_winning_position(my_future_positions):
+                return move
+        return -1
 
-def num_to_symbol(num: int) -> str:
-    if num == EMPTY:
-        return " "
-    elif num == ME:
-        return "X"
-    elif num == THEM:
-        return "O"
-    else:
-        assert False
-
-
-# Prints to stderr to not confuse the manager
-def print_board():
-    for row in range(0, 3):
-        print("|".join([num_to_symbol(val) for val in board[row]]), file=sys.stderr)
-        if row != 2:
-            print("-----", file=sys.stderr)
-
-
-def get_board(numpad_coord: int) -> int:
-    return board[numpad_coord // 3][numpad_coord % 3]
-
-
-def set_board(numpad_coord: int, value: int):
-    board[numpad_coord // 3][numpad_coord % 3] = value
-
-
-def numpad_coord_of(row: int, col: int) -> int:
-    return row * 3 + col
-
-
-# Return a winning move or -1 if no winning move exists
-def move_that_wins() -> int:
-    for row in range(0, 3):
-        row_iterable = board[row]
-        if sum(row_iterable) == 2:
-            return numpad_coord_of(row, row_iterable.index(min(row_iterable)))
-    for col in range(0, 3):
-        col_iterable = [board[row][col] for row in range(0, 3)]
-        if sum(col_iterable) == 2:
-            return numpad_coord_of(col_iterable.index(min(col_iterable)), col)
-    nw_se_iterable = [board[x][x] for x in range(0, 3)]
-    if sum(nw_se_iterable) == 2:
-        x = nw_se_iterable.index(min(nw_se_iterable))
-        return numpad_coord_of(x, x)
-    ne_sw_iterable = [board[x][2 - x] for x in range(0, 3)]
-    if sum(ne_sw_iterable) == 2:
-        x = ne_sw_iterable.index(min(ne_sw_iterable))
-        return numpad_coord_of(x, 2 - x)
-    return -1
+    my_turn = bool(int(input()))
+    while True:
+        print_board()
+        if has_winning_position(my_positions):
+            print("I won!!", file=sys.stderr)
+            sys.exit(0)
+        elif has_winning_position(their_positions):
+            print("I lost :(", file=sys.stderr)
+            sys.exit(0)
+        elif len(empty_positions) == 0:
+            print("Tie :o", file=sys.stderr)
+            sys.exit(0)
+        if not my_turn:
+            opponent_move = int(input())
+            assert opponent_move in empty_positions
+            empty_positions.remove(opponent_move)
+            their_positions.add(opponent_move)
+        else:
+            move = move_that_wins()
+            if move < 0:  # when no move that wins, get arbitrary move
+                move = empty_positions.pop()
+            my_positions.add(move)
+            print(move, flush=True)
+        my_turn = not my_turn
 
 
-my_turn = bool(int(input()))
-while True:
-    print_board()
-    winner = calculate_winner()
-    if winner == ME:
-        print("I won!!", file=sys.stderr)
-        sys.exit(0)
-    elif winner == THEM:
-        print("I lost :(", file=sys.stderr)
-        sys.exit(0)
-    if not my_turn:
-        opponent_move = int(input())
-        current_val = get_board(opponent_move)
-        assert current_val == EMPTY
-        set_board(opponent_move, THEM)
-        my_turn = True
-    else:
-        move = move_that_wins()
-        while move < 0 or get_board(move) != EMPTY:
-            move = random.randint(0, 8)
-        print(move, flush=True)
-        set_board(move, ME)
-        my_turn = False
+if __name__ == "__main__":
+    main()
